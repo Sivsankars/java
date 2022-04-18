cc7bb8aa444dea7694ab037a4f12e0b2b4002fc2
Sivasankar. S <96108681+Sivsankars@users.noreply.github.com>
2022-04-18 11:29:42 +0530

    Update Stringmanupliation.java

--- c02/Stringmanupliation.java
+++ c02/Stringmanupliation.java
@@ -4,7 +4,12 @@ public class Stringmanupliation {
 	class compare{
 		compare(String s,String y){
 			System.out.println("comparing........");
-			System.out.println(s+"compare to"+y+"="+s.equals(y));
+			System.out.println(s+" compare to "+y+"="+s.equals(y));
+		}
+	}
+	class concat{
+		concat(String s,String y){
+			System.out.println(s+y);
 		}
 	}
 
@@ -12,16 +17,23 @@ public class Stringmanupliation {
 		int i=1;
 		Scanner obj= new Scanner(System.in);
 		Stringmanupliation s= new Stringmanupliation();
+		System.out.println("enter the first String");
+		String s1=obj.nextLine();
+		System.out.println("enter the second  String");
+		String s2=obj.nextLine();
 		while(i==1){
-			System.out.println("1.compare\n2.concatination\nenter your choice");
+			System.out.println("1.compare\n2.concatination\nexit\nenter your choice");
 			int c=obj.nextInt();
 			switch(c){
 			case 1:
-				System.out.println("enter the first String");
-				String s1=obj.nextLine();
-				System.out.println("enter the second  String");
-				String s2=obj.nextLine();
+				
 				Stringmanupliation.compare r= s.new compare(s1,s2);
+				break;
+			case 2:
+				Stringmanupliation.concat x= s.new concat(s1,s2);
+				break;
+			case 3:
+				i=0;
 				break;
 			}
 
@@ -29,3 +41,4 @@ public class Stringmanupliation {
 	}
 
 }
+
